7a02b766478c7d4938590a1948264852143c7057
Atharva Parkale <atharva.parkale@gmail.com>
2022-04-27 10:39:25 +0530

    Image size reduced

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,6 +9,7 @@ function Home() {
     <div className="home_section">
       <h1>Project done by Atharva Parkale</h1>
       <span>This website is not responsive, prefer opening it on PC</span>
+      <h2>Some images may take time to load due to their bigger size , be patient !</h2>
     </div>
     </>
   )
